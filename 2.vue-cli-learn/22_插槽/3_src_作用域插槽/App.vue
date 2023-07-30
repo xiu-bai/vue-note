c72a4e9a79f686a89362c115b4f264f8b803b931
小白 <1163070111@qq.com>
2023-07-30 23:50:21 +0800

    作用域插槽

---
+++ 2.vue-cli-learn/22_插槽/3_src_作用域插槽/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <Category title="游戏">
+      <template scope="{games}">
+        <!--这里data为插槽给你传递的对象包含你所需要的数据 包装成对象的原因就是你当时可能个插槽传递了多个数据-->
+        <ul>
+          <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+        </ul>
+      </template>
+    </Category>
+
+    <Category title="游戏">
+      <template scope="{games,msg}">
+        <!--这里data为插槽给你传递的对象包含你所需要的数据-->
+        <ol>
+          <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+        </ol>
+        <h3>{{ msg }}</h3>
+      </template>
+    </Category>
+
+    <Category title="游戏">
+      <template slot-scope="{games}">
+        <h4 v-for="(item, index) in games" :key="index">{{ item }}</h4>
+      </template>
+    </Category>
+  </div>
+</template>
+
+<script>
+import Category from "@/components/Category";
+export default {
+  name: "App",
+  components: {
+    Category
+  },
+}
+</script>
+<style lang="css" scoped>
+.container,
+.foot {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
