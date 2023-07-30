f7acecd1e68beefc9d27cf6561b229e23c259655
小白 <1163070111@qq.com>
2023-07-30 21:54:36 +0800

    配置代理服务器

---
+++ 2.vue-cli-learn/19_src_配置代理服务器/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="getStudents">获取学生信息</button>
+    <button @click="getCars">获取学生信息</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "App",
+  components: {
+    
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    getStudents(){
+      axios.get('http://localhost:8080/api1/students')
+      .then(res => console.log(res.data))
+      .catch(e => console.log(`msg: ${e.message}, stack: ${e.stack}`));
+    },
+    getCars(){
+      axios.get('http://localhost:8080/api2/cars')
+      .then(res => console.log(res.data))
+      .catch(e => console.log(`msg: ${e.message}, stack: ${e.stack}`));
+    }
+
+  },
+  mounted() {
+    
+  },
+}
+</script>
+<style>
+</style>
--- 2.vue-cli-learn/vue.config.js
+++ 2.vue-cli-learn/vue.config.js
@@ -1,8 +1,39 @@
-const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+  pages: {
+    index: {
+      // 入口
+      entry: 'src/main.js',
+    },
+  },
 
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  /*关闭语法检查*/
-  lintOnSave: false
-})
+  lintOnSave: false,//关闭语法检查
+  //开启代理服务器(方式一)
+  /*devServer:{
+      proxy: 'http://localhost:5000', 
+      //将请求转发给端口号5001，注意这里
+      //①只能配置单个代理，不能配置多个代理
+      //②不能灵活的控制请求是否走代理
+  }*/
+  //开启代理服务器(方式二) 多个代理
+  devServer: {
+    proxy: {
+      //当请求的前缀是api，直接转发请求到服务器5001端口
+      '/api1': {
+        target: 'http://localhost:5000',
+        ws: true, //用于支持websocket
+        changeOrigin: true, //用于控制请求头中host的值
+        pathRewrite: {
+          //把请求中含有的api替换成空字符串
+          '^/api1': '',
+        }
+      },
+      '/api2': {
+        target: 'http://localhost:5001',
+        //不写 ws和changeOrigin默认为true
+        pathRewrite: {
+          '^/api2': '',
+        }
+      }
+    }
+  }
+}
