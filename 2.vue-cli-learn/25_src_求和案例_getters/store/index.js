29279f94b1534697b85d6952c4b923029c415643
小白 <1163070111@qq.com>
2023-08-02 23:32:46 +0800

    求和案例-getter

---
+++ 2.vue-cli-learn/25_src_求和案例_getters/store/index.js
@@ -0,0 +1,68 @@
+// 该文件用于创建Vuex中最为核心的store
+import Vue from "vue";
+// 引入Vuex
+import Vuex from "vuex";
+// 使用Vuex插件
+Vue.use(Vuex)
+
+// 准备actions - 用于响应组件中的动作
+const actions = {
+    //收到上下文对象(包含commit)和dispatch过来的值
+    // jia(context,value){
+    //     context.commit('JIA',value)
+    // },
+    // jian(context,value){
+    //     context.commit('JIAN',value)
+    // },
+    jiaOdd(context, value) {
+        if (context.state.sum % 2) {
+            context.commit('JIA', value)
+            // context.state.sum += 1;//这样可以实现但是记住本次对状态的改变开发者工具将无法捕获，因为开发者工具是对mutations对话的
+        }
+    },
+    jianWait(context, value) {
+        setTimeout(() => {
+            context.commit('JIA', value)
+        }, 500)
+    },
+    // demo1(context, value){
+    //     console.log('处理了一些事情---demo1');
+    //     context.dispatch('demo2', value);
+    // },
+    // demo2(context, value){
+    //     console.log('在action的demo中完成最终的逻辑');
+    //     if(context.state.sum % 2) {
+    //         console.log('@')
+    //         context.commit('INCREMENT',value);
+    //     }
+    // }
+};
+// 准备mutations - 用于操作数据（state）
+const mutations = {
+    //收到state和要操作数value
+    JIA(state, value) {
+        state.sum += value;
+    },
+    JIAN(state, value) {
+        state.sum -= value;
+    },
+};
+// 准备state - 用于存储数据
+const state = {
+    sum: 0, //当前的和
+};
+
+// 准备getters - 用于将state中的数据进行加工
+const getters = {
+    bigSum(state){
+        return state.sum * 10
+    }
+}
+
+// 创建并暴露store
+export default new Vuex.Store({
+    actions,
+    mutations,
+    state,
+    getters
+})
