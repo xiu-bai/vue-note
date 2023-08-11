fedc044182ccd15e42179179b07aafd24d7fc72b
小白 <1163070111@qq.com>
2023-08-11 08:11:31 +0800

    验证history和hash两种模式在部署在后端浏览器(express)会出现的问题

---
+++ 2.vue-cli-learn/43_src_history模式和hash模式/pages/About.vue
@@ -0,0 +1,32 @@
+<template>
+    <h2>我是About的内容</h2>
+</template>
+  
+<script>
+export default {
+    name: "About",
+    //组件内路由守卫
+
+    //与前置和后置是两码事
+    //通过路由规则进入该组件时被调用,注意一定是通过路由规则进入组件，普通的组件装载是不会调用的
+    beforeRouteEnter(to, from, next) {
+        console.log('App---beforeRouteEnter');
+        console.log(from, to);
+        if (to.meta.isAuth) {
+            //代表需要鉴权
+            if (localStorage.getItem('school') === 'beijing') next();
+            else alert('无权限');
+        } else {
+            next();
+        }
+    },
+    //通过路由规则离开该组件时被调用
+    beforeRouteLeave(to, from, next) {
+        console.log('App---beforeRouteLeave');
+        console.log(from, to);
+        next();
+    }
+}
+</script>
+  
+<style scoped></style>
