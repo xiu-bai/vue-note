79fda3fd87e726f623d62157b5fdb8a7ec419d6f
xiu-bai <1163070111@qq.com>
2023-08-13 22:51:07 +0800

    setup的两个注意点

---
+++ 3.vue3_learn/06_src_setup的两个注意点/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Demo @hello="showHelloMsg" msg="你好" school="北京">
+    <template v-slot:name>
+      <span>123</span>
+    </template>
+    <template v-slot:asd>
+      <span>567</span>
+    </template>
+  </Demo>
+</template>
+
+<script>
+import Demo from './compontent/Demo'
+export default{
+  name:'App',
+  components:{Demo},
+  setup(){
+    function showHelloMsg(value){
+      alert(`你好，${value}`)
+    }
+
+    return{
+      showHelloMsg
+    }
+  }
+}
+</script>
---
+++ 3.vue3_learn/06_src_setup的两个注意点/compontent/Demo.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>一个人的信息</h1>
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <slot name="name"></slot>
+    <button @click="text">测试Demo组件的hello事件</button>
+    <slot name="asd"></slot>
+</template>
+  
+<script>
+import { reactive } from 'vue';
+export default {
+    name: 'App',
+    props:['msg','school'],
+    emits:['hello'],
+    setup(props,context) {
+        // console.log(this); ///undefined
+        // console.log(props); //props: 外部给组件丢的参数 => 响应式(Proxy实例)
+        // console.log(context.attrs);// 相当于vue2中的$attrs
+        // console.log(context.emit);// 触发自定义事件
+        console.log(context.slots);//插槽
+        console.log(context);
+        let person = reactive({
+            name: '修白',
+            age: '18',
+        })
+
+        // 方法
+        function text(){
+            context.emit('hello',666)
+        }
+
+        //返回一个对象
+        return {
+            person,
+            text
+        }
+    }
+}
+</script>
