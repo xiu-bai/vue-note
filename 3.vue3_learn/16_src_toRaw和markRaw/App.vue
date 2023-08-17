3da5b1ad564f6e930d76c91c067ae740c8890e72
小白 <1163070111@qq.com>
2023-08-17 22:49:44 +0800

    toRaw和markRaw

---
+++ 3.vue3_learn/16_src_toRaw和markRaw/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Demo v-if="isShowDemo"/>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Demo from './compontent/Demo'
+export default{
+  name:'App',
+  components:{Demo},
+  setup() {
+    let isShowDemo = ref(true);
+    return{isShowDemo}
+  }
+}
+</script>
---
+++ 3.vue3_learn/16_src_toRaw和markRaw/compontent/Demo.vue
@@ -0,0 +1,71 @@
+<template>
+    <h2>当前求和为:{{ sum }}</h2>
+    <button @click="sum++">sum+1</button>
+    <hr />
+
+    <h2>{{ person }}</h2>
+    <h2>姓名:{{ name }}</h2>
+    <h2>年龄:{{ age }}</h2>
+    <h2>薪资:{{ job.j1.salary }}块</h2>
+    <h3 v-show="person.car">座驾信息:{{ person.car }}</h3>
+    <button @click="name = name + '~'">修改姓名</button>
+    <button @click="age++">增长年龄</button>
+    <button @click="job.j1.salary++">增长薪资</button>
+    <button @click="showRawPerson">输出最原始的person</button>
+    <button @click="addCar">给人添加一台车</button>
+    <button @click="person.car && (person.car.name += '!')">换车名</button>
+    <button @click="changePrice">换价格</button>
+</template>
+
+<script>
+import { ref, reactive, toRefs, toRaw, markRaw } from 'vue';
+export default {
+    name: 'App',
+    setup() {
+        let sum = ref(0);
+
+        let person = reactive({
+            name: '张三',
+            age: 18,
+            job: {
+                j1: {
+                    salary: 1800
+                }
+            }
+        });
+
+        //ref reactive(响应式)
+
+        const showRawPerson = () => {
+            // console.log(person); //proxy代理对象 Proxy {....}
+            const p = toRaw(person);
+            p.age++;//注意此时页面不会再发生变化了,普普通通的对象不是响应式
+            console.log(p); //原始对象数据  {....}
+
+            // const sum = toRaw(sum);
+            // console.log(sum); //undefined //这条路走不通,toRaw只处理reactive对象
+        }
+
+        const addCar = () => {
+            person.car = markRaw({
+                name: '捷达',
+                price: 40
+            }); //在响应式的对象身上添加任何属性都是响应式的，经过markRaw一包装就变成最原始的数据就不会再做响应
+        }
+
+        const changePrice = () => {
+            person.car?.price && person.car.price++;
+            console.log(person?.car?.price);
+        }
+
+        return {
+            sum,
+            person,
+            ...toRefs(person),
+            showRawPerson,
+            addCar,
+            changePrice
+        };
+    }
+}
+</script>
