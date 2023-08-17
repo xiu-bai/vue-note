c60a822faffe986bdcfeab79bfa40ee80384bc74
小白 <1163070111@qq.com>
2023-08-17 23:42:46 +0800

    响应式数据的判断

---
+++ 3.vue3_learn/19_src_响应式数据的判断/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <h2>我是App组件</h2>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs, readonly, isRef, isReactive, isReadonly, isProxy } from "vue";
+export default {
+  name: 'App',
+  setup() {
+    let car = reactive({ name: '奔驰', price: '40' })
+    let sum = ref(0)
+    let car2 = readonly(car)
+
+    console.log('sum,ref,isRef---' + isRef(sum));
+    console.log('car,reactive,reactive---' + isReactive(car));
+    console.log('car2,readonly,isReadonly---' + isReadonly(car2));
+    console.log('car,reactive,isProxy---' + isProxy(car));
+    console.log('car2,readonly,isProxy---' + isProxy(car2));
+
+    return {
+      ...toRefs(car),
+      sum,
+      car2
+
+    }
+  }
+}
+</script>
