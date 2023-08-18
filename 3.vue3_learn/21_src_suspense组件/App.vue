8f1cf30d711f713c7f5ad6f7e11f90b015a12c3f
小白 <1163070111@qq.com>
2023-08-18 23:51:57 +0800

    suspense组件

---
+++ 3.vue3_learn/21_src_suspense组件/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <h2>我是APP组件</h2>
+    <Suspense>
+      <template v-slot:default>
+        <Child />
+      </template>
+      <template v-slot:fallback>
+        <h3>加载中</h3>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script>
+// import Child from "./components/Child";//静态引入
+import { defineAsyncComponent } from 'vue';
+const Child = defineAsyncComponent(() => import('./components/Child'))//动态引入
+
+export default {
+  name: 'App',
+  components: { Child },
+  setup() {
+
+  }
+}
+</script>
+
+<style>
+.app {
+  background: grey;
+  padding: 10px;
+}
+</style>
