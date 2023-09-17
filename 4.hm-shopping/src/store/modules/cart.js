eb98a00f6d273c2563231bea0aa2057a7bec61a4
xiu-bai <1163070111@qq.com>
2023-09-18 06:45:27 +0800

    购物车

--- 4.hm-shopping/src/store/modules/cart.js
+++ 4.hm-shopping/src/store/modules/cart.js
@@ -5,18 +5,23 @@ export default {
     namespaced: true,
     state() {
         return {
-            catList: []
+            cartList: []
         }
     },
     mutations: {
-        setCartList(state,newList){
-            state.catList = newList
-        }
+        setCartList(state, newList) {
+            state.cartList = newList
+        },
+        toggleCheck(state, goodsId) {
+            // 让对应的 id 的项 状态取反
+            const goods = state.cartList.find(item => item.goods_id === goodsId)
+            goods.isChecked = !goods.isChecked
+        },
     },
     actions: {
         async getCartAction(context) {
             const { data } = await getCartList();
-            console.log(data);
+            // console.log(data);
             // 后台返回的数据中，不包含复选框的选中状态，为了实现将来的功能
             // 需要手动维护数据，给每一项，添加一个 isChecked 状态 (标记当前商品是否选中)
             data.list.forEach(item => {
@@ -25,6 +30,29 @@ export default {
             context.commit('setCartList', data.list)
         },
     },
-    getters: {}
+    getters: {
+        // 求所有的商品累加总数
+        cartTotal(state) {
+            return state.cartList.reduce((sum, item) => sum + item.goods_num, 0)
+        },
+        // 选中的商品项
+        selCartList(state) {
+            return state.cartList.filter(item => item.isChecked)
+        },
+        // 选中的总数
+        selCount(state, getters) {
+            return getters.selCartList.reduce((sum, item) => sum + item.goods_num, 0)
+        },
+        // 选中的总价
+        selPrice(state, getters) {
+            return getters.selCartList.reduce((sum, item) => {
+                return sum + item.goods_num * item.goods.goods_price_min
+            }, 0).toFixed(2)
+        },
+        // 是否全选
+        isAllChecked(state) {
+            return state.cartList.every(item => item.isChecked)
+        }
+    }
 
 }
--- 4.hm-shopping/src/views/layout/cart.vue
+++ 4.hm-shopping/src/views/layout/cart.vue
@@ -3,7 +3,7 @@
         <van-nav-bar title="购物车" fixed />
         <!-- 购物车开头 -->
         <div class="cart-title">
-            <span class="all">共<i>4</i>件商品</span>
+            <span class="all">共<i>{{ cartTotal }}</i>件商品</span>
             <span class="edit">
                 <van-icon name="edit" />
                 编辑
@@ -12,17 +12,15 @@
 
         <!-- 购物车列表 -->
         <div class="cart-list">
-            <div class="cart-item" v-for="item in 10" :key="item">
-                <van-checkbox></van-checkbox>
+            <div class="cart-item" v-for="item in cartList" :key="item.goods_id">
+                <van-checkbox @click="toggleCheck(item.goods_id)" :value="item.isChecked"></van-checkbox>
                 <div class="show">
-                    <img src="http://cba.itlike.com/public/uploads/10001/20230321/a072ef0eef1648a5c4eae81fad1b7583.jpg"
-                        alt="">
+                    <img :src="item.goods.goods_image" alt="">
                 </div>
                 <div class="info">
-                    <span class="tit text-ellipsis-2">新Pad 14英寸 12+128 远峰蓝 M6平板电脑 智能安卓娱乐十核游戏学习二合一 低蓝光护眼超清4K全面三星屏5GWIFI全网通
-                        蓝魔快本平板</span>
+                    <span class="tit text-ellipsis-2">{{ item.goods.goods_name }}</span>
                     <span class="bottom">
-                        <div class="price">¥ <span>1247.04</span></div>
+                        <div class="price">¥ <span>{{ item.goods.goods_price_min }}</span></div>
                         <!-- 既希望保留原本的形参，又需要通过调用函数传参 => 箭头函数包装一层 -->
                         <CountBox @input="(value) => changeCount(value, item.goods_id, item.goods_sku_id)"
                             :value="item.goods_num"></CountBox>
@@ -33,17 +31,17 @@
 
         <div class="footer-fixed">
             <div class="all-check">
-                <van-checkbox icon-size="18"></van-checkbox>
+                <van-checkbox :value="isAllChecked" icon-size="18"></van-checkbox>
                 全选
             </div>
 
             <div class="all-total">
                 <div class="price">
                     <span>合计：</span>
-                    <span>¥ <i class="totalPrice">99.99</i></span>
+                    <span>¥ <i class="totalPrice">{{ selPrice }}</i></span>
                 </div>
-                <div v-if="true" class="goPay">结算(5)</div>
-                <div v-else class="delete">删除</div>
+                <div v-if="true" class="goPay" :class="{ disabled: selCount === 0 }">结算({{ selCount }})</div>
+                <div v-else class="delete" :class="{ disabled: selCount === 0 }">删除</div>
             </div>
         </div>
     </div>
@@ -51,18 +49,27 @@
   
 <script>
 import CountBox from '@/components/CountBox.vue'
-import store from '@/store'
+import { mapState, mapGetters } from 'vuex'
 export default {
     name: 'CartPage',
     components: {
         CountBox
     },
+    computed: {
+        ...mapState('cart', ['cartList']),
+        ...mapGetters('cart', ['cartTotal', 'selCartList', 'selCount', 'selPrice', 'isAllChecked']),
+    },
     created() {
         // 必须是登录过的用户，才能用户购物车列表
-        if (store.getters.token) {
+        if (this.$store.getters.token) {
             this.$store.dispatch('cart/getCartAction')
         }
     },
+    methods: {
+        toggleCheck(goodsId) {
+            this.$store.commit('cart/toggleCheck', goodsId)
+        },
+    }
 }
 </script>
   
