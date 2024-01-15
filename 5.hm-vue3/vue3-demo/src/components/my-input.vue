b4581892c1437ca2c89853d0e67b85bf1151c8b6
xiu-bai <1163070111@qq.com>
2024-01-15 20:21:47 +0800

    defineOptions和defineModel

---
+++ 5.hm-vue3/vue3-demo/src/components/my-input.vue
@@ -0,0 +1,17 @@
+<script setup>
+// defineProps({
+//     modelValue: String
+// })
+// const emit = defineEmits(['update:modelValue'])
+
+const modelValue = defineModel()
+</script>
+
+<template>
+    <div>
+        <input type="text" :value="modelValue" @input="e => modelValue = e.target.value">
+    </div>
+</template>
+
+
+
---
+++ 5.hm-vue3/vue3-demo/src/views/register/index.vue
@@ -0,0 +1,17 @@
+<!-- <script>
+export default {
+    name: 'RegisterIndex'
+}
+</script> -->
+
+<script setup>
+defineOptions({
+    name: 'RegisterIndex'
+})
+</script>
+
+<template>
+    <div>
+        我是注册页
+    </div>
+</template>
