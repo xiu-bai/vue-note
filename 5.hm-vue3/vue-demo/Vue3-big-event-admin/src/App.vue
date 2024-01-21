1506c202f971a478811dcf28ed63ff5f5919e9ae
xiu-bai <1163070111@qq.com>
2024-01-21 20:21:06 +0800

    element plus组件

--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/App.vue
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/App.vue
@@ -18,9 +18,15 @@ const goList = () => {
 
 <template>
   <div>
+    <textDemo />
     我是app
     <button @click="$router.push('./home')">aaaa</button>
     <button @click="goList">bbbb</button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
   </div>
 </template>
 
