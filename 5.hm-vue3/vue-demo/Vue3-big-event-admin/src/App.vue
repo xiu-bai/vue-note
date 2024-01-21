1a79c77383f98307923abdf10cf00675a46d1eb8
xiu-bai <1163070111@qq.com>
2024-01-21 20:09:52 +0800

    router配置

--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/App.vue
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/App.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+
+// 在Vue3 CompositionAPI 中
+// 1.获取路由对象 router useRouter
+//   const router = useRouter()
+// 2.获取路由参数route useRoute
+//   const route = useRoute()
+
+const router = useRouter()
+const route = useRoute()
+
+const goList = () => {
+  router.push('/list')
+  console.log(router, route)
+}
+</script>
 
 <template>
-  <div>我是app</div>
+  <div>
+    我是app
+    <button @click="$router.push('./home')">aaaa</button>
+    <button @click="goList">bbbb</button>
+  </div>
 </template>
 
 <style scoped></style>
--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/router/index.js
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// createRouter 创建路由实例
+// 配置 history 模式
+// 1. history模式：createWebHistory     地址栏不带 #
+// 2. hash模式：   createWebHashHistory 地址栏带 #
+// console.log(import.meta.env.DEV)
+
+// vite 中的环境变量 import.meta.env.BASE_URL  就是 vite.config.js 中的 base 配置项
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: []
