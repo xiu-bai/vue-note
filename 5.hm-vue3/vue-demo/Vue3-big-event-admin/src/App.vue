a08e5264edbb8f987676028513be15caaec4ebdd
xiu-bai <1163070111@qq.com>
2024-01-21 21:07:50 +0800

    1.pinia单独抽离到stores下的index。2.在仓库中统一进行导出

--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/App.vue
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
-import { useUserStore } from '@/stores/user'
+import { useUserStore, useCountStore } from '@/stores'
 
 // 在Vue3 CompositionAPI 中
 // 1.获取路由对象 router useRouter
@@ -17,6 +17,7 @@ const goList = () => {
 }
 
 const userStore = useUserStore()
+const countStore = useCountStore()
 </script>
 
 <template>
@@ -30,7 +31,11 @@ const userStore = useUserStore()
       >登录</el-button
     >
     <el-button @click="userStore.removeToken()" type="success">退出</el-button>
+    <hr />
+    {{ countStore.count }}
+    <el-button @click="countStore.add(2)">加法2</el-button>
   </div>
 </template>
 
 <style scoped></style>
+@/stores/modules/user
