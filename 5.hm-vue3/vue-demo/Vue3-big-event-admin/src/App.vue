f40736b8534cac94735dd3db1299da050e537811
xiu-bai <1163070111@qq.com>
2024-01-23 21:09:04 +0800

    登录页静态

--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/App.vue
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/App.vue
@@ -1,47 +1,10 @@
-<script setup>
-import { useRoute, useRouter } from 'vue-router'
-import { useUserStore, useCountStore } from '@/stores'
-
-// 在Vue3 CompositionAPI 中
-// 1.获取路由对象 router useRouter
-//   const router = useRouter()
-// 2.获取路由参数route useRoute
-//   const route = useRoute()
-
-const router = useRouter()
-const route = useRoute()
-
-const goList = () => {
-  router.push('/list')
-  console.log(router, route)
-}
-
-const userStore = useUserStore()
-const countStore = useCountStore()
-</script>
+<script setup></script>
 
 <template>
   <div>
-    <hr />
+    <!-- App.vue只需要留一个路由出口 router-view即可 -->
     <router-view></router-view>
-    <hr />
-    <hr />
-    <hr />
-    <hr />
-    <textDemo />
-    我是app
-    <button @click="$router.push('./home')">aaaa</button>
-    <button @click="goList">bbbb</button>
-    <p>{{ userStore.token }}</p>
-    <el-button @click="userStore.setToken('00000000000000000')" type="primary"
-      >登录</el-button
-    >
-    <el-button @click="userStore.removeToken()" type="success">退出</el-button>
-    <hr />
-    {{ countStore.count }}
-    <el-button @click="countStore.add(2)">加法2</el-button>
   </div>
 </template>
 
 <style scoped></style>
-@/stores/modules/user
--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/views/login/LoginPage.vue
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/views/login/LoginPage.vue
@@ -1,3 +1,112 @@
+<script setup>
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const isRegister = ref(true)
+</script>
+
 <template>
-  <div>登录页</div>
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <el-form ref="form" size="large" autocomplete="off" v-if="isRegister">
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入再次密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>
+            注册
+          </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = false">
+            ← 返回
+          </el-link>
+        </el-form-item>
+      </el-form>
+      <el-form ref="form" size="large" autocomplete="off" v-else>
+        <el-form-item>
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            name="password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space
+            >登录</el-button
+          >
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true">
+            注册 →
+          </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
 </template>
+
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  background-color: #fff;
+
+  .bg {
+    background:
+      url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+      url('@/assets/login_bg.jpg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+
+    .title {
+      margin: 0 auto;
+    }
+
+    .button {
+      width: 100%;
+    }
+
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
