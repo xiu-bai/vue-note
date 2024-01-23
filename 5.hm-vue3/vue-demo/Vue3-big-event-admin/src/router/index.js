8ea162785dcc32a996f3ab7943f2405090496d0a
xiu-bai <1163070111@qq.com>
2024-01-23 22:56:33 +0800

    登录访问拦截

--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/router/index.js
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import { useUserStore } from '@/stores/index'
 // createRouter 创建路由实例
 // 配置 history 模式
 // 1. history模式：createWebHistory     地址栏不带 #
@@ -41,4 +41,16 @@ const router = createRouter({
   ]
 })
 
+// 登录访问拦截 => 默认是直接放行的
+// 根据返回值决定，是放行还是拦截
+// 返回值：
+// 1. undefined / true  直接放行
+// 2. false 拦回from的地址页面
+// 3. 具体路径 或 路径对象  拦截到对应的地址
+// '/login'   { name: 'login' }
+router.beforeEach((to) => {
+  // 如果没有token, 且访问的是非登录页，拦截到登录，其他情况正常放行
+  const useStore = useUserStore()
+  if (!useStore.token && to.path !== '/login') return '/login'
+})
 export default router
