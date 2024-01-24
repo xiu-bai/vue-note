f2301314141f67db1f5ff3fcfc27d88cead0b324
xiu-bai <1163070111@qq.com>
2024-01-24 22:24:23 +0800

    搭建架子、封装成组件

--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/views/article/ArticleChannel.vue
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/views/article/ArticleChannel.vue
@@ -1,1 +1,10 @@
-<template>频道管理</template>
+<script setup></script>
+<template>
+  <page-container title="文章分类">
+    <template #extra>
+      <el-button @click="">添加分类</el-button>
+    </template>
+  </page-container>
+</template>
+
+<style lang="scss" scoped></style>
--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/views/article/ArticleManage.vue
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/views/article/ArticleManage.vue
@@ -1,1 +1,10 @@
-<template>文章管理</template>
+<script setup></script>
+<template>
+  <page-container title="文章管理">
+    <template #extra>
+      <el-button @click="">添加文章</el-button>
+    </template>
+  </page-container>
+</template>
+
+<style lang="scss" scoped></style>
