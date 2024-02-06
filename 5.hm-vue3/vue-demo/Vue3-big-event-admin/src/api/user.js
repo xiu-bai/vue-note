d22107db42d5b434a37530c783f73c0fc7fb486d
xiu-bai <1163070111@qq.com>
2024-02-06 14:19:40 +0800

    重置密码

--- 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/api/user.js
+++ 5.hm-vue3/vue-demo/Vue3-big-event-admin/src/api/user.js
@@ -18,3 +18,7 @@ export const userUpdateInfoService = ({ id, nickname, email }) =>
 // 上传头像
 export const userUploadAvatarService = (avatar) =>
   request.patch('/my/update/avatar', { avatar })
+
+// 重置密码
+export const userUpdatePassService = ({ old_pwd, new_pwd, re_pwd }) =>
+  request.patch('/my/updatepwd', { old_pwd, new_pwd, re_pwd })
